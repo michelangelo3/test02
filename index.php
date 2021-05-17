5d57ab504b8f498c6fbef2fddbd05a8f6e7f9130
michelangelo3 <michelangelo3@wolke7.net>
2021-05-17 00:02:31 +0000

    index.php hinzugefügt

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+ <head>
+  <title>PHP-Test</title>
+ </head>
+ <body>
+ <?php start(); ?>
+ <br>
+ <a href="test1.php">test1</a>
+ </body>
+</html>
+
+<?php
+function start() {
+    print('hello, this is index.php');
+}
+?>
